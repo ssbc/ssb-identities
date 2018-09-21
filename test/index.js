5aedb1209c906e3969d683af02c97943293b53e4
Dominic Tarr <dominic.tarr@gmail.com>
2018-09-21 12:38:59 +1200

    test coverage for creating encrypted messages and decrypting them with other keys

--- test/index.js
+++ test/index.js
@@ -15,6 +15,7 @@ var sbot = Scuttlebot({
   }
 })
 
+
 tape('list identities', function (t) {
   sbot.identities.create(function (err, id) {
     if(err) throw err
@@ -37,7 +38,6 @@ tape('list identities', function (t) {
 tape('publish as', function (t) {
   sbot.identities.list(function (err, ls) {
     if(err) throw err
-    console.log('KEYS', ls)
     cont.para(
       ls.map(function (id) {
         return function (cb) {
@@ -120,9 +120,37 @@ tape('double safety on private, 3', function (t) {
   })
 })
 
+tape("can read something encrypted so that the main sbot can't read", function (t) {
+  sbot.identities.list(function (err, ls) {
+    var id = ls[1]
+    sbot.identities.publishAs({
+      id: id,
+      content: {
+        type: 'private',
+        //incorrect key! so this should error
+        recps: [id],
+        text: 'secrets'
+      },
+      private: true
+    }, function (err, msg) {
+      if(err) throw err
+      t.notOk(err)
+      console.log(msg)
+      t.equal(typeof msg.value.content, 'string')
+      sbot.get({id: msg.key, private: true}, function (err, msg) {
+        if(err) throw err
+        t.deepEqual(msg.content, {type: 'private', recps: [id], text: 'secrets'})
+        t.end()
+      })
+    })
+  })
+
+})
+
 tape('close', function (t) {
   sbot.close()
   t.end()
 })
+
 
 
